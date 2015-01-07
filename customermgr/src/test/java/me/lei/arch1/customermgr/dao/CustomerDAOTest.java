9b8cf48288f2e61575827e9abf1f653435c17048
qilei <qilei2010@gmail.com>
2015-01-08 06:35:58 +0800

    dao

--- customermgr/src/test/java/me/lei/arch1/customermgr/dao/CustomerDAOTest.java
+++ customermgr/src/test/java/me/lei/arch1/customermgr/dao/CustomerDAOTest.java
@@ -1,13 +1,24 @@
 package me.lei.arch1.customermgr.dao;
 
+import me.lei.arch1.customermgr.vo.CustomerModel;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
 
 /**
  * Created by qilei on 2015/1/5.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "")
 public class CustomerDAOTest {
+    @Resource
+    private CustomerDAO customerDAO;
+
     @Test
     public void testGetByCustomerId() throws Exception {
-
+        CustomerModel customerModel = customerDAO.getByCustomerId("c1");
     }
 }

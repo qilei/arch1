7aae5cb3400de96bdb85bcededc29a74040f50c9
qilei <qilei@zuche.com>
2015-02-09 17:25:37 +0800

    加入分页插件

---
+++ common/src/main/java/me/lei/pagination/dialect/DBMS.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright © 2012-2013 mumu@yfyang. All Rights Reserved.
+ */
+
+package me.lei.pagination.dialect;
+
+/**
+ * <p>
+ * 数据库类型.
+ * </p>
+ *
+ * @author poplar.yfyang
+ * @version 1.0 2012-05-08 上午11:36
+ * @since JDK 1.5
+ */
+public enum DBMS {
+    MYSQL,
+    ORACLE,
+    DB2,
+    H2,
+    HSQL,
+    POSTGRE,
+    SQLSERVER,
+    SQLSERVER2005,
+    SYBASE,
+    DERBY,
+    /**
+     * 自定义
+     */
+    EX
+}
